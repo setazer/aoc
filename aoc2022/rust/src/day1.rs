5c2649feddd9a8cffeed07e6fa5370fd656072c8
SetazeR <mail@setazer.me>
2022-12-01 16:22:58 +0400

    Refactor Day 1 + Template

--- aoc2022/rust/src/day1.rs
+++ aoc2022/rust/src/day1.rs
@@ -1,31 +1,34 @@
 
-fn parse_elves_data(input: &str) -> Vec<i32> {
+type SolutionType = i32;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<SolutionType> {
     let elves_data: Vec<&str> = input.split("\n\n").collect();
-    let elves_data: Vec<i32> = elves_data.iter().map(|x| x.split("\n").map(|x|x.parse::<i32>().unwrap()).sum()).collect();
+    let elves_data: Vec<SolutionType> = elves_data.iter()
+        .map(|x| x
+            .split("\n")
+            .map(|x|x.parse::<SolutionType>().unwrap())
+            .sum())
+        .collect();
     elves_data
 }
 
-#[aoc(day1, part1, Chars)]
-pub fn part1(input: &str) -> i32 {
-    let elves_data = parse_elves_data(input);
-    *elves_data.iter().max().expect("Has max always")
+#[aoc(day1, part1)]
+pub fn part1(data: &[SolutionType]) -> SolutionType {
+    *data.iter().max().expect("Has max always")
 }
 
-#[aoc(day1, part2, Chars)]
-pub fn part2(input: &str) -> i32 {
-    let mut elves_data = parse_elves_data(input);
+#[aoc(day1, part2)]
+pub fn part2(data: &[SolutionType]) -> SolutionType {
+    let mut elves_data: Vec<_> = data.into();
     elves_data.sort();
-    elves_data.reverse();
-    elves_data[..3].iter().sum()
+    elves_data.iter().rev().take(3).sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{part1, part2};
-
-    #[test]
-    fn sample1() {
-        assert_eq!(part1("1000
+    use super::{input_generator, part1, part2};
+    const TEST_INPUT:&str = "1000
 2000
 3000
 
@@ -38,25 +41,15 @@ mod tests {
 8000
 9000
 
-10000"), 24000);
+10000";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(&input_generator(TEST_INPUT)) , 24000);
     }
 
     #[test]
     fn sample2() {
-        assert_eq!(part2("1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000"), 45000);
+        assert_eq!(part2(&input_generator(TEST_INPUT)), 45000);
     }
-
 }
---
+++ aoc2022/rust/src/daytemplate.rs
@@ -0,0 +1,33 @@
+
+type SolutionType = i32;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<SolutionType> {
+    input.lines()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(data: &[SolutionType]) -> SolutionType {
+    0
+}
+
+#[aoc(day1, part2)]
+pub fn part2(data: &[SolutionType]) -> SolutionType {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{input_generator, part1, part2};
+    const TEST_INPUT:&str = "";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(&input_generator(TEST_INPUT)) , 0);
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(&input_generator(TEST_INPUT)), 0);
+    }
+}
