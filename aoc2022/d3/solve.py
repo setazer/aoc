a287f85f481978565d4bf0159b469619cbc63b4c
SetazeR <mail@setazer.me>
2022-12-03 13:08:21 +0400

    Day 3

--- aoc2022/d3/solve.py
+++ aoc2022/d3/solve.py
@@ -1,29 +1,60 @@
-from aocframework import AoCFramework
+from functools import reduce
+from itertools import tee
 
+from aocframework import AoCFramework
+from string import ascii_letters
+
+
+PRIORITIES = [None, *ascii_letters]
 
 class DayPart1(AoCFramework):
     test_cases = (
-        # ('', ),
+        ('''vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw''', 157),
     )
-    known_result = None
+    known_result = 7727
 
     def go(self):
         raw_split = self.linesplitted
-        return
+        sum = 0
+        for rucksack in raw_split:
+            half = len(rucksack)//2
+            comp1, comp2 = set(rucksack[:half]), set(rucksack[half:])
+            common_item = tuple(comp1.intersection(comp2))[0]
+            sum += ascii_letters.index(common_item) + 1
 
+        return sum
 
 DayPart1()
 
 
+def triplets(iterable):
+    a, b, c = [iter(iterable)]*3
+    return zip(a, b, c)
+
+
 class DayPart2(AoCFramework):
     test_cases = (
-        # ('', ),
+        ('''vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw''', 70),
     )
-    known_result = None
+    known_result = 2609
 
     def go(self):
         raw_split = self.linesplitted
-        return
+        sum = 0
+        for lines3 in triplets(raw_split):
+            common_item = tuple(reduce(lambda x,y: x & y, map(set, lines3)))[0]
+            sum += ascii_letters.index(common_item) + 1
+        return sum
 
 
 DayPart2()
---
+++ aoc2022/rust/src/day3.rs
@@ -0,0 +1,59 @@
+use std::collections::HashSet;
+
+type SolutionType = i32;
+const PRIORITY: &str = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+#[aoc(day3, part1)]
+pub fn part1(input: &str) -> SolutionType {
+    let mut data:Vec<_> = vec![];
+    for line in input.lines() {
+        let half:usize = line.len()/2;
+        data.push(line.split_at(half))
+    }
+    let mut sum:_ = 0;
+    for (half1, half2) in data {
+        let hs1 = half1.chars().collect::<HashSet<_>>();
+        let hs2 = half2.chars().collect::<HashSet<_>>();
+        let inter = hs1.intersection(&hs2).into_iter().next().unwrap();
+        let priority = PRIORITY.find(*inter).unwrap() as SolutionType;
+        sum+=priority;
+    }
+    sum
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> SolutionType {
+    let data:Vec<_> = input.lines().collect();
+    let mut sum:SolutionType = 0;
+    for chunk in data.chunks_exact(3) {
+        let elem = chunk.iter()
+            .map(|&x| x.chars().collect::<HashSet<_>>())
+            .reduce(|set1, set2| set1.intersection( &set2).map(|&x| x).collect()).unwrap();
+        sum+=PRIORITY.find(*elem.iter().next().unwrap()).unwrap() as i32;
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+         part1,
+         part2,
+    };
+    const TEST_INPUT:&str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(TEST_INPUT) , 157);
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(TEST_INPUT), 70);
+    }
+}
