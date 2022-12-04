29bedcfdeb3864f6717ab7f3c3edc72dc6fee77a
SetazeR <mail@setazer.me>
2022-12-04 18:16:26 +0400

    Day 4

--- aoc2022/d4/solve.py
+++ aoc2022/d4/solve.py
@@ -1,15 +1,53 @@
 from aocframework import AoCFramework
 
 
+class Range:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+
+    @classmethod
+    def from_string(cls, range_string):
+        start = int(range_string.partition('-')[0])
+        end = int(range_string.partition('-')[-1])
+        return cls(start, end)
+
+    def __contains__(self, other):
+        if isinstance(other, Range):
+            return self.start <= other.start and self.end >= other.end
+        raise TypeError()
+
+    def __and__(self, other):
+        if isinstance(other, Range):
+            return (
+                    other in self
+                    or self in other
+                    or self.start <= other.start <= self.end
+                    or self.start <= other.end <= self.end
+                    or other.start <= self.start <= other.end
+                    or other.start <= self.end <= other.end
+            )
+
+
 class DayPart1(AoCFramework):
     test_cases = (
-        # ('', ),
+        ('''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8''', 2),
     )
-    known_result = None
+    known_result = 496
 
     def go(self):
         raw_split = self.linesplitted
-        return
+        sum = 0
+        for line in raw_split:
+            range1, _, range2 = line.partition(',')
+            range1, range2 = Range.from_string(range1), Range.from_string(range2)
+            sum += range1 in range2 or range2 in range1
+        return sum
 
 
 DayPart1()
@@ -17,13 +55,23 @@ DayPart1()
 
 class DayPart2(AoCFramework):
     test_cases = (
-        # ('', ),
+        ('''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8''', 4),
     )
-    known_result = None
+    known_result = 847
 
     def go(self):
         raw_split = self.linesplitted
-        return
+        sum = 0
+        for line in raw_split:
+            range1, _, range2 = line.partition(',')
+            range1, range2 = Range.from_string(range1), Range.from_string(range2)
+            sum += range1 & range2
+        return sum
 
 
 DayPart2()
---
+++ aoc2022/rust/src/day4.rs
@@ -0,0 +1,74 @@
+
+type SolutionType = i32;
+
+pub struct Range{
+    start: i32,
+    end: i32,
+}
+
+impl Range {
+    fn from_string(s:&str) -> Self {
+        let (start, end) = s.split_once('-').unwrap();
+        Range {
+            start:start.parse().unwrap(),
+            end:end.parse().unwrap(),
+        }
+
+    }
+    fn contains(&self, other: &Range) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+    fn intersects(&self, other: &Range) -> bool {
+        other.contains(self)
+        || self.contains(other)
+        || (self.start <= other.start && other.start <= self.end)
+        || (self.start <= other.end && other.end <= self.end)
+        || (other.start <= self.start && self.start <= other.end)
+        || (other.start <= self.end && self.end <= other.end)
+    }
+}
+
+#[aoc_generator(day4)]
+pub fn input_generator(input: &str) -> Vec<(Range,Range)> {
+    let mut res:Vec<_> = vec![];
+    for line in input.lines() {
+        let (r1, r2) = line.split_once(',').unwrap();
+        res.push((Range::from_string(r1), Range::from_string(r2)))
+    }
+    res
+}
+
+#[aoc(day4, part1)]
+pub fn part1(data: &[(Range, Range)]) -> SolutionType {
+    data.iter().map(|(r1,r2)| (r1.contains(&r2) || r2.contains(&r1)) as i32).sum()
+}
+
+#[aoc(day4, part2)]
+pub fn part2(data: &[(Range, Range)]) -> SolutionType {
+    data.iter().map(|(r1,r2)| r1.intersects(&r2) as i32).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        input_generator,
+        part1,
+        part2,
+    };
+    const TEST_INPUT:&str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(&input_generator(TEST_INPUT)) , 2);
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(&input_generator(TEST_INPUT)), 4);
+    }
+}
